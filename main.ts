878183dd88b24b500d2d31882a68f8ec48f9752f
Bluebaritone21 <82056947+Bluebaritone21@users.noreply.github.com>
2021-04-28 09:21:22 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
 basic.forever(function () {
-	
+    if (input.compassHeading() <= 0 && 0 <= input.compassHeading()) {
+        basic.showArrow(ArrowNames.North)
+    } else if (input.compassHeading() < 135 && 45 < input.compassHeading()) {
+        basic.showArrow(ArrowNames.East)
+    } else if (false) {
+        basic.showArrow(ArrowNames.South)
+    } else {
+        basic.showArrow(ArrowNames.West)
+    }
 })
