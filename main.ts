4d42f736483cf653d8e4b4c6c5c5f7222b4bd9ad
Bluebaritone21 <82056947+Bluebaritone21@users.noreply.github.com>
2021-04-28 10:50:20 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
 basic.forever(function () {
     if (input.compassHeading() >= 315 || 45 >= input.compassHeading()) {
         basic.showArrow(ArrowNames.North)
+        serial.writeLine("N")
     } else if (input.compassHeading() < 135 && 45 < input.compassHeading()) {
-        basic.showArrow(ArrowNames.East)
+        basic.showArrow(ArrowNames.West)
+        serial.writeLine("E")
     } else if (input.compassHeading() <= 225 && 135 <= input.compassHeading()) {
         basic.showArrow(ArrowNames.South)
+        serial.writeLine("S")
     } else {
-        basic.showArrow(ArrowNames.West)
+        basic.showArrow(ArrowNames.East)
+        serial.writeLine("W")
     }
 })
